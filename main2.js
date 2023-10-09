9d055e97af852c19bee00e8c5bca360a93cbb680
ProfessorGabriel123 <126835277+ProfessorGabriel123@users.noreply.github.com>
2023-10-09 14:44:36 +0000

    asdsadsad

--- main2.js
+++ main2.js
@@ -1,5 +1,8 @@
 function tocasomclap(){
     document.querySelector('#som_tecla_clap').play()
 }
-
+function tocasomtim (){
+    document.querySelector('#som_tecla_tim').play()
+}
 document.querySelector('.tecla_clap').onclick = tocasomclap
+ducument.querySelector('.tecla_tim').onclick = tocasomtim
